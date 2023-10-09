1eb7b41e5ce9f940ab82fca03166664e86c517d6
nelel <nesams@taltech.ee>
2023-10-09 20:30:54 +0300

    Code cleanup

---
+++ src/molecules/JokeCard.vue
@@ -0,0 +1,56 @@
+<template>
+  <v-col cols="12" sm="6" md="4">
+    <v-card color="secondary">
+      <v-card-title class="card-title">{{ joke.setup }}</v-card-title>
+      <v-card-subtitle class="card-subtitle">Hind: {{ joke.price }}€</v-card-subtitle>
+      <v-card-actions>
+        <v-btn color="primary" class="action-btn" @click="$emit('openDialog', joke.id)">
+          <span v-if="joke.showPunchline">Vaata</span>
+          <span v-else>Osta</span>
+        </v-btn>
+      </v-card-actions>
+    </v-card>
+  </v-col>
+</template>
+
+<script setup lang="ts">
+import {defineProps, toRefs} from 'vue';
+interface Joke {
+  id: number;
+  setup: string;
+  punchline: string;
+  price: number;
+  timesBought: number;
+  showPunchline?: boolean;
+}
+
+const props = defineProps<{
+  joke: Joke;
+}>();
+
+const { joke } = toRefs(props);
+</script>
+<style scoped>
+
+.card_title {
+  color: white;
+  padding-top: 15px;
+  padding-left: 22px;
+}
+
+.card_subtitle {
+  padding-left: 22px;
+  padding-bottom: 5px;
+  color: white;
+}
+
+.v_btn {
+  margin-left: 4px;
+  margin-bottom: 5px;
+  color: white;
+}
+
+.joke_card {
+  min-width: 400px;
+}
+</style>
--- src/pages/AllJokes.vue
+++ src/pages/AllJokes.vue
@@ -1,6 +1,29 @@
+<template>
+  <v-container class="all-jokes">
+    <div class="header-title">
+      <h1>Müügil olevad naljad</h1>
+    </div>
+    <v-row>
+      <JokeCard
+        v-for="joke in jokes"
+        :key="joke.id"
+        :joke="joke"
+        @openDialog="openDialog"
+      />
+    </v-row>
+    <JokeDialog
+      v-if="showDialog"
+      :joke="currentDialogJoke"
+      @closeDialog="closeDialog"
+      @buyJoke="buyJoke"
+    />
+  </v-container>
+</template>
 <script setup lang="ts">
-import {onMounted, Ref, ref} from "vue";
+import {computed, onMounted, Ref, ref} from "vue";
 import axios from "axios";
+import JokeCard from "@/molecules/JokeCard.vue";
+import JokeDialog from "@/molecules/JokeDialog.vue";
 
 const jokes_url: string = "http://localhost:8080/api/jokes/setups";
 const buy_url: string = "http://localhost:8080/api/jokes/buy/";
@@ -12,7 +35,7 @@ interface Joke {
   punchline: string;
   price: number;
   timesBought: number;
-  showDialog?: boolean;
+  showDialog: boolean;
   showPunchline?: boolean;
 }
 
@@ -26,9 +49,14 @@ const currentDialogJoke = ref<Joke>({
   showPunchline: false
 });
 
+const showDialog = computed(() => {
+  return currentDialogJoke.value.showDialog;
+})
+
 const openDialog = (id: number) => {
   setCurrentJokeById(id);
   currentDialogJoke.value.showDialog = true;
+
 }
 
 const closeDialog = () => {
@@ -43,7 +71,6 @@ const buyJoke = async (id: number) => {
   try {
     console.log("Buying joke")
     const response = await axios.get(buy_url + id);
-    console.log(response.data)
     currentDialogJoke.value.punchline = response.data.punchline;
     currentDialogJoke.value.showPunchline = true
   } catch (error) {
@@ -67,88 +94,8 @@ onMounted(() => {
 });
 </script>
 
-<template>
-  <v-container>
-    <h1 class="h1">Müügil olevad naljad</h1>
-    <v-row>
-      <v-col
-        v-for="joke in jokes.values()"
-        :key="joke.id"
-        cols="12"
-        sm="6"
-        md="4"
-      >
-        <v-card color="secondary">
-          <v-card-title class="card_title">
-            {{ joke.setup }}
-          </v-card-title>
-          <v-card-subtitle class="card_subtitle">
-            Hind: {{ joke.price }}€
-          </v-card-subtitle>
-          <v-card-actions>
-            <v-btn
-              color="primary"
-              class="v_btn"
-              @click="openDialog(joke.id)"
-            >
-              <span v-if="joke.showPunchline">Vaata</span>
-              <span v-else>Osta</span>
-
-              <v-dialog
-                v-model="joke.showDialog"
-                width="auto"
-              >
-                <v-card
-                  color="accent"
-                  class="joke_card"
-                >
-                  <v-card-text>{{ currentDialogJoke.setup }}</v-card-text>
-                  <v-card-text v-if="currentDialogJoke.showPunchline">{{ joke.punchline }}</v-card-text>
-                  <v-card-text v-else>Soovid osta selle nalja lööklauset? Hind: {{ joke.price }}€</v-card-text>
-                  <v-card-actions>
-                    <v-btn color="primary"
-                           class="v_btn"
-                           @click="buyJoke(joke.id)"
-                           v-if="!currentDialogJoke.showPunchline">
-                      Osta
-                    </v-btn>
-                    <v-btn color="error" @click="closeDialog">Välju</v-btn>
-                  </v-card-actions>
-                </v-card>
-              </v-dialog>
-            </v-btn>
-          </v-card-actions>
-        </v-card>
-      </v-col>
-    </v-row>
-  </v-container>
-</template>
-
 <style scoped>
-.h1 {
-  padding-bottom: 1rem;
-  color: white;
-}
-
-.card_title {
-  color: white;
-  padding-top: 15px;
-  padding-left: 22px;
-}
-
-.card_subtitle {
-  padding-left: 22px;
-  padding-bottom: 5px;
-  color: white;
-}
-
-.v_btn {
-  margin-left: 4px;
-  margin-bottom: 5px;
-  color: white;
-}
-
-.joke_card {
-  min-width: 400px;
+.header-title {
+  padding-bottom: 10px;
 }
 </style>
