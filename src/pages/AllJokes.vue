a3b5cc7ce1073d83763eeae1e0d72f0a13d9a140
inpihl <antspihl@gmail.com>
2023-10-09 13:03:00 +0300

    Added dialog window for seeing the whole punchline and a "Buy punchline" button which shows the user the punchline

--- src/pages/AllJokes.vue
+++ src/pages/AllJokes.vue
@@ -11,6 +11,35 @@ interface Joke {
   punchline: string;
   price: number;
   timesBought: number;
+  showDialog?: boolean;
+  showPunchline?: boolean;
+}
+
+const currentJoke = ref<Joke>({
+  id: 0,
+  setup: "",
+  punchline: "",
+  price: 0,
+  timesBought: 0,
+  showDialog: false,
+  showPunchline: false
+});
+
+const openDialog = (id: number) => {
+  setCurrentJokeById(id);
+  currentJoke.value.showDialog = true;
+}
+
+const closeDialog = () => {
+  currentJoke.value.showDialog = false;
+}
+
+const setCurrentJokeById = (id: number) => {
+  currentJoke.value = jokes.value.find(joke => joke.id === id)!;
+}
+
+const buyJoke = async (id: number) => {
+  currentJoke.value.showPunchline = true
 }
 
 const getJokes = async () => {
@@ -24,7 +53,6 @@ const getJokes = async () => {
 }
 
 onMounted(() => {
-  console.log("Mounted")
   getJokes();
 });
 </script>
@@ -51,8 +79,33 @@ onMounted(() => {
             <v-btn
               color="primary"
               class="v_btn"
+              @click="openDialog(joke.id)"
             >
-              Osta
+              <span v-if="joke.showPunchline">Vaata</span>
+              <span v-else>Osta</span>
+
+              <v-dialog
+                v-model="joke.showDialog"
+                width="auto"
+              >
+                <v-card
+                  color="accent"
+                  class="joke_card"
+                >
+                  <v-card-text>{{ currentJoke.setup }}</v-card-text>
+                  <v-card-text v-if="currentJoke.showPunchline">{{ joke.punchline }}</v-card-text>
+                  <v-card-text v-else>Soovid osta selle nalja lööklauset? Hind: {{ joke.price }}€</v-card-text>
+                  <v-card-actions>
+                    <v-btn color="primary"
+                           class="v_btn"
+                           @click="buyJoke(joke.id)"
+                           v-if="!currentJoke.showPunchline">
+                      Osta
+                    </v-btn>
+                    <v-btn color="error" @click="closeDialog">Välju</v-btn>
+                  </v-card-actions>
+                </v-card>
+              </v-dialog>
             </v-btn>
           </v-card-actions>
         </v-card>
@@ -82,5 +135,9 @@ onMounted(() => {
   margin-left: 4px;
   margin-bottom: 5px;
   color: white;
+}
+
+.joke_card {
+  min-width: 400px;
 }
 </style>
