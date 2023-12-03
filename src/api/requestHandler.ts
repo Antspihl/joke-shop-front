9f12822944380b79b02fabf653ac4ee5434ea3ad
nelel <nesams@taltech.ee>
2023-12-03 20:03:59 +0200

    Removed unused requests from requestHandler

--- src/api/requestHandler.ts
+++ src/api/requestHandler.ts
@@ -1,28 +1,7 @@
-import {Joke, Login, Register} from "@/molecules/types";
 import axios from "axios";
 
 export const API_URL: string = "http://193.40.156.35:8080/api";
 export const API_HEADERS: {} = {'content-type': 'application/json'};
-export async function fetchSetups() {
-  try {
-    const response = await axios.get(API_URL + "/jokes/setups");
-    return response.data;
-
-  } catch (error) {
-    console.error("Error fetching setups", error);
-  }
-}
-
-export async function fetchTop3Setups() {
-  try {
-    const response = await axios.get(API_URL + "/jokes/top3");
-    return response.data;
-
-  } catch (error) {
-    console.error("Error fetching top 3 setups", error);
-  }
-}
-
 export async function buyJokeWithId(id: number) {
   try {
     const response = await axios.get(API_URL + "/jokes/buy/" + id);
@@ -30,39 +9,5 @@ export async function buyJokeWithId(id: number) {
 
   } catch (error) {
     console.error("Error buying joke", error);
-  }
-}
-
-export async function fetchUsers() {
-  try {
-    const response = await axios.get(API_URL + "/users/all");
-    return response.data;
-
-  } catch (error) {
-    console.error("Error fetching users", error);
-  }
-}
-
-export async function registerUser(register: Register) {
-  try {
-    const response = await axios.post(
-      API_URL + "/auth/register",
-      JSON.stringify(register)
-    );
-
-  } catch (error) {
-    console.error("Error registering user", error);
-  }
-}
-
-export async function login(login: Login) {
-  try {
-    const response = await axios.post(
-      API_URL + "/auth/login",
-      JSON.stringify(login)
-    );
-
-  } catch (error) {
-    console.error("Error logging in", error);
   }
 }
